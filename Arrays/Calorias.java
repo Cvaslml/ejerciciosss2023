714e3d407d8a64ade02c8db73ecfafad6b294684
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2023-03-02 06:34:07 -0500

    Update Calorias.java

--- Arrays/Calorias.java
+++ Arrays/Calorias.java
@@ -1,17 +1,27 @@
+package ggg;
+
 import javax.swing.JOptionPane;
 
-public class Calorias{
-    public static void main(String[] args) {
-        // Variable
+public class Calorias
+{
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null, "Tu programa: Contador de Calorías");
+
         int[] calorias;
-        int n;
+        String[] Dia = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
 
-        calorias= new int[6];
+        calorias = new int[7];
 
-        for (int i=0; i < calorias.length; i++){
-            n = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el valor de calorias día*día: "));
-            calorias[i] = n;
+        for (int i = 0; i < calorias.length; i++)
+        {
+            calorias[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de calorias para el dia " + Dia[i] + ": ", "CONTEO DE CALORIAS"));
         }
-        JOptionPane.showMessageDialog(null, "Calorías:" + "\nDía Lunes: " + calorias[0] + "Día Martes" + calorias[1] + "Día Miércoles: " + calorias[2] + "Día Jueves" + calorias[3] + "Día Viernes" + calorias[4] + "Día Sabado: " + calorias[5] + "Día Domingo:" + calorias[6]);
+
+        for(int j = 0; j < calorias.length; j++)
+        {
+            JOptionPane.showMessageDialog(null, "El numero de calorias para el dia " + Dia[j] + ": " + calorias[j]);
+
+        }
     }
 }
